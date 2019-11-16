76a583eb16a3da05426c9c65fbf485dd68d89021
michal-puk <puk.michal@seznam.cz>
2019-11-16 11:26:06 +0100

    you can win

--- app/src/main/java/cz/ludogry/textadventures/monderong/navigation/VictoryFragment.kt
+++ app/src/main/java/cz/ludogry/textadventures/monderong/navigation/VictoryFragment.kt
@@ -1,14 +1,15 @@
 package cz.ludogry.textadventures.monderong.navigation
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import cz.ludogry.textadventures.monderong.R
+import cz.ludogry.textadventures.monderong.databinding.FragmentVictoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,6 +30,8 @@ class VictoryFragment : Fragment() {
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
+    private lateinit var binding: FragmentVictoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -41,8 +44,15 @@ class VictoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_victory, container, false)
+        binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_victory, container, false)
+
+        binding.victoryDescription.text = getString(R.string.youWon)
+        binding.backToMenu.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_victoryFragment_to_mainMenuFragment)
+        )
+
+        return binding.root
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -50,14 +60,14 @@ class VictoryFragment : Fragment() {
         listener?.onFragmentInteraction(uri)
     }
 
-    override fun onAttach(context: Context) {
+    /*override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnFragmentInteractionListener) {
             listener = context
         } else {
             throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
-    }
+    }*/
 
     override fun onDetach() {
         super.onDetach()
